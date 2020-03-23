6e94c6a6c46570d8f4fa171619f8afe79963fe69
igorashs <igorashsirbu@gmail.com>
2020-03-24 00:03:15 +0200

    add procfile and update port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const indexRouter = require('./routes/index');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ? process.env.PORT : 3000;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -11,6 +11,4 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 app.use('/', indexRouter);
 
-app.listen(process.env.PORT ? process.env.PORT : PORT, () =>
-  console.log(`Server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
