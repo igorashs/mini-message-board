0772bedae689f24b5a0db01bdbf87e731cdf3e24
igorashs <igorashsirbu@gmail.com>
2020-03-23 23:49:54 +0200

    add style

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const PORT = 3000;
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+app.use(express.static('public'));
 app.use('/', indexRouter);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(process.env.PORT ? process.env.PORT : PORT, () =>
+  console.log(`Server is running on port ${PORT}`)
+);
